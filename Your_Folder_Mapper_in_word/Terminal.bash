49ad0da45bd7f5268b2a2f82a3c27d1f8ad9253a
Sara Aghamiri <42683308+SaraAghamiri@users.noreply.github.com>
2025-05-20 15:01:14 -0500

    Create Terminal.bash

---
+++ Your_Folder_Mapper_in_word/Terminal.bash
@@ -0,0 +1,12 @@
+#bash:
+#nano file.py
+
+#bash:
+#Python file.py 
+#--------
+
+#bash
+#>> python -m venv venv_docx
+#>> source venv_docx/bin/activate
+#>> pip install python-docx
+#>> python make_doc.py
